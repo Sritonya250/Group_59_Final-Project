35ff2c0ff6b7cbf77d4de6564d617ba262fdb3b7
BharathKumarT007 <tamilselvam.b@northeastern.edu>
2024-12-06 13:41:44 -0500

    CSR code added

--- CareConnect/src/model/csr/OpportunityDirectory.java
+++ CareConnect/src/model/csr/OpportunityDirectory.java
@@ -1,30 +1,30 @@
-///*
-// * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-// * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
-// */
-//package model.csr;
-//
-//import java.util.ArrayList;
-//
-///**
-// *
-// * @author tbhar
-// */
-//public class OpportunityDirectory {
-//    ArrayList<Opportunity> opportunityList;
-//
-//    public OpportunityDirectory() {
-//        opportunityList = new ArrayList<Opportunity>();
-//    }
-//
-//    public ArrayList<Opportunity> getOpportunityList() {
-//        return opportunityList;
-//    }
-//    
-//    public Opportunity createNewOpportunity(CSREmployerProfile crsEmployerProfile, String jobTitle, EmployableSkills requiredSkill, String jobDesc, int availablePositions ){
-//        Opportunity o = new Opportunity(crsEmployerProfile, jobTitle, requiredSkill, jobDesc, availablePositions);
-//        opportunityList.add(o);
-//        return o;
-//    }
-//    
-//}
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.csr;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author tbhar
+ */
+public class OpportunityDirectory {
+    ArrayList<Opportunity> opportunityList;
+
+    public OpportunityDirectory() {
+        opportunityList = new ArrayList<Opportunity>();
+    }
+
+    public ArrayList<Opportunity> getOpportunityList() {
+        return opportunityList;
+    }
+    
+    public Opportunity createNewOpportunity(CSREmployerProfile crsEmployerProfile, String jobTitle, EmployableSkills requiredSkill, String jobDesc, int availablePositions ){
+        Opportunity o = new Opportunity(crsEmployerProfile, jobTitle, requiredSkill, jobDesc, availablePositions);
+        opportunityList.add(o);
+        return o;
+    }
+    
+}
